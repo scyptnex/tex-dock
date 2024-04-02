ad947dce8b5ea430a7934276ceb141f517d78c80
Scyptnex <nic.h.misc@gmail.com>
2024-04-02 14:55:02 +1100

    shell lint (kindof)

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 function main() {
-    project_dir="$(readlink -f "$(dirname "$(dirname "$0")")")"
-    docker build ${project_dir} -t tex-dock:auto
+    local project_dir="$(readlink -f "$(dirname "$(dirname "$0")")")"
+    docker build "${project_dir}" -t tex-dock:auto
 }
 
 main "$@"
